96657733daf78b214400eb7bce343646dbd9ac65
neko113 <alstn113@gmail.com>
2022-07-09 01:48:53 +0900

    feat : prisma module화

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
 import configuration from './config/configuration';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -11,8 +14,11 @@ import configuration from './config/configuration';
       isGlobal: true,
       load: [configuration],
     }),
+    PrismaModule,
     AuthModule,
     UserModule,
+    PostModule,
+    CommentModule,
   ],
   providers: [
     {
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,10 +4,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { setupSwagger } from './utils/setupSwagger';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const prismaService = app.get(PrismaService);
   const port = configService.get<number>('server.port');
   const logger = new Logger();
 
@@ -18,6 +20,7 @@ async function bootstrap() {
   });
 
   setupSwagger(app);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(port);
   logger.verbose(`Listening On Port ${port}`);
 }
