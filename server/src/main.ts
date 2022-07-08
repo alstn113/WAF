3c70ace725c37982774ca2992b82c123430c6c71
neko113 <alstn113@gmail.com>
2022-07-09 01:23:54 +0900

    feat : prisma 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,12 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { PrismaService } from 'prisma/prisma.service';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const prismaService = app.get(PrismaService);
   const port = configService.get<number>('server.port');
   const logger = new Logger();
 
@@ -18,7 +16,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await prismaService.enableShutdownHooks(app);
   await app.listen(port);
 
   logger.verbose(`Listening On Port ${port}`);
