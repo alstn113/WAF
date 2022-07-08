0d07e56017576ab4e31a8b1fb7d6220abceddb4b
neko113 <alstn113@gmail.com>
2022-07-09 01:00:47 +0900

    feat : server settings

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    AuthModule,
+    UserModule,
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,23 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { PrismaService } from 'prisma/prisma.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const configService = app.get(ConfigService);
+  const prismaService = app.get(PrismaService);
+  const port = configService.get<number>('server.port');
+  const logger = new Logger();
+
+  app.enableCors({
+    origin: configService.get<string>('client'),
+    credentials: true,
+  });
+
+  await prismaService.enableShutdownHooks(app);
+  await app.listen(port);
+  logger.verbose(`Listening On Port ${port}`);
 }
 bootstrap();
