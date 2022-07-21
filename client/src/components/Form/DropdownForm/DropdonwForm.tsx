f7ac126fb1f8ad9b726edd842837b26aea927ddd
neko113 <alstn113@gmail.com>
2022-07-21 17:06:12 +0900

    feat : add answerList dnd

--- client/src/components/Form/DropdownForm/DropdonwForm.tsx
+++ client/src/components/Form/DropdownForm/DropdonwForm.tsx
@@ -1,6 +1,13 @@
 import Button from '@src/components/common/Button/Button';
 import { Input } from '@src/components/common/TextInput/TextInput.styles';
 import userFormBuilderStore from '@src/libs/store/useFormBuilderStore';
+import reorder from '@src/libs/utils/reorder';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from 'react-beautiful-dnd';
 import * as S from './DropdownForm.styles';
 
 interface Props {
@@ -26,22 +33,63 @@ const DropdownForm = ({ index }: Props) => {
     newAnswerList[answerIndex] = data;
     changeOfferedAnswer(index, newAnswerList);
   };
+
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+    if (!destination) return;
+    if (destination.index === source.index) return;
+
+    const newAnswerList = reorder(answerList, source.index, destination.index);
+    changeOfferedAnswer(index, newAnswerList);
+  };
   return (
     <div>
       <div>
         <S.Flex>
-          <div>
-            {answerList?.map((answer, answerIndex) => (
-              <div key={answerIndex}>
-                <Input
-                  type="text"
-                  value={answer}
-                  onChange={(e) => onChangeItem(answerIndex, e.target.value)}
-                />
-                <button onClick={() => onDelete(answerIndex)}>삭제</button>
-              </div>
-            ))}
-          </div>
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="drop">
+              {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {answerList?.map((answerItem, answerItemIndex) => {
+                    return (
+                      <Draggable
+                        draggableId={String(answerItemIndex)}
+                        index={answerItemIndex}
+                      >
+                        {(provided) => (
+                          <div
+                            {...provided.draggableProps}
+                            ref={provided.innerRef}
+                          >
+                            <div
+                              {...provided.dragHandleProps}
+                              style={{
+                                width: '100%',
+                                height: '10px',
+                                backgroundColor: 'rgb(228, 143, 143)',
+                              }}
+                            />
+                            ⏺
+                            <Input
+                              type="text"
+                              value={answerItem}
+                              onChange={(e) =>
+                                onChangeItem(answerItemIndex, e.target.value)
+                              }
+                            />
+                            <button onClick={() => onDelete(answerItemIndex)}>
+                              삭제
+                            </button>
+                          </div>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
           <Button onClick={onCreate}>옵션 생성</Button>
         </S.Flex>
       </div>
--- client/src/libs/utils/reorder.ts
+++ client/src/libs/utils/reorder.ts
@@ -1,6 +1,4 @@
-import { IForm } from '@libs/interfaces';
-
-const reorder = (list: IForm[], startIndex: number, endIndex: number) => {
+const reorder = (list: any[], startIndex: number, endIndex: number) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
